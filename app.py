2ef8d8df011eaf7258ff44184de8fd26e271c2a4
Paul <paulinnorway@gmail.com>
2020-07-18 14:25:07 +0000

    Import request, jsonify. Add get_ingredients view

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import pymysql
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, jsonify
 
 app = Flask(__name__)
 
@@ -40,6 +40,37 @@ def recipes():
 def ingredients():
     return render_template("ingredients.html")
 
+@app.route('/get_ingredients', methods=['POST'])
+def get_ingredients():
+    #get data from request object
+#    ingredient = request.form.to_dict()
+
+    ingredient = request.form['ingredient_name']
+    #https://www.tutorialspoint.com/best-way-to-test-if-a-row-exists-in-a-mysql-table#:~:text=To%20test%20whether%20a%20row,false%20is%20represented%20as%200.
+    sql_read = "SELECT name, COUNT(*) FROM ingredient WHERE name = '"+ingredient+"'GROUP BY name;"
+#    sql = "SELECT EXISTS(SELECT * FROM ingredient WHERE name = '"+ingredient+"');"
+
+    try:
+        # Connect to the database
+        connection = pymysql.connect(host='localhost',
+                                     user=username,
+                                     password=password,
+                                     db='vmpdb')
+
+        # Run a query
+        with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+            cursor.execute(sql_read)
+            result = cursor.fetchall() #returns a dictionary
+            #connection.commit();
+            if result:
+                print('result is:')
+                print(result)
+    finally:
+        # Close the connection, regardless of whether or not the above was successful
+        connection.close()
+
+#    return jsonify({'found': result}) #dictionary -> json
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
