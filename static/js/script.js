f6e88c0fd986c681952c6d205863d2eadfc43d5b
Paul <paulinnorway@gmail.com>
2020-07-19 18:44:33 +0000

    Define and call getNames() function

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
 
   if (location.href.match(/ingredients/)) {
     console.log("Found ingredients page");
+    getNames('ingredient', 'name');
   } else if (location.href.match(/recipes/)) {
     console.log("Found recipes page");
   }
@@ -34,6 +35,36 @@ $(document).ready(function () {
       },
     });
   });
+
+  function getNames(table, column, event) {
+    event.preventDefault();
+    //get names of ingredients/recipes
+    $.ajax({
+      //create an ajax request to get_ingredients()
+      data: {
+        //data that gets sent to python
+        table: table,
+        column: column
+      },
+      type: "POST",
+      dataType: "json",
+      url: "get_names",
+      success: function (result, status, xhr) {
+        console.log("returned data: ", result);
+        if (result[0]) {
+          //found in database
+          console.log("Found " + result); //object
+          return result[0];
+        } else {
+          console.log("Couldn't find");
+        }
+      },
+      error: function (xhr, status, error) {
+        console.log("Error");
+      },
+    });
+  }
+
   /*    let mydata = {
       "aa": null,
       "ab": null,
