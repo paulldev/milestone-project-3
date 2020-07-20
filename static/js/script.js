fe1cd91e891b0ab8d62ba1b78edc3681fac208b2
Paul <paulinnorway@gmail.com>
2020-07-20 16:19:18 +0000

    Move getNames() near the top. Temporarily comment out unused functions

--- static/js/script.js
+++ static/js/script.js
@@ -2,20 +2,56 @@ $(document).ready(function () {
   $(".button-collapse").sideNav();
   $("select").material_select();
 
+    function getNames(table, column) {
+    console.log("3. inside getNames(). AJAX call starting");
+    console.log("3. using table ("+table+") and column ("+column+")");
+    //get names of ingredients/recipes
+    $.ajax({ //create an ajax request to get_names()
+      data: { //data that gets sent to python
+        table: table,
+        column: column
+      },
+      type: "POST",
+      dataType: "json",
+      url: "/get_names",
+      success: function (result, status, xhr) {
+        console.log("4. [success:] successful ajax call");
+        console.log("4. ajax type returned (result): ", typeof result);
+        console.log("4. ajax type returned (result[0]): ", typeof result[0]);
+        console.log("4. ajax result (stringify): ", JSON.stringify(result));
+        if (result) {
+          //found in database
+          console.log("5. Return result:", result); //object
+          //return JSON.parse(result);
+          return result.responseText;
+        } else {
+          console.log("5. Couldn't find");
+        }
+      },
+      error: function (xhr, status, error) {
+        console.log("4. [Error:] Error??", xhr);
+      },
+      complete: function (result) {
+        console.log("AJAX call complete", result.responseText);
+      }
+    });
+    console.log("6. end of getNames(). AJAX call ending");
+  }
+
   if (location.href.match(/ingredients/)) {
-    console.log("0. Found ingredients page");
-    console.log("1. Calling getNames() ");
-//      debugger;
+    console.log("1. Found ingredients page");
+    console.log("2. Calling getNames() ");
+      //debugger;
 
     mydata = getNames("ingredient", "name");
-    console.log("Back from calling allNames() >>>>>>", mydata);
+    console.log("** Back from calling getNames() >>>>>>", mydata);
   } else if (location.href.match(/recipes/)) {
     console.log("Found recipes page");
   } else {
     console.log("Error, unknown page");
   }
 
-  $("#ingredient_name").on("keyup", function (event) {
+/*  $("#ingredient_name").on("keyup", function (event) {
     event.preventDefault();
     //check if ingredient exists
     $.ajax({
@@ -40,47 +76,15 @@ $(document).ready(function () {
         console.log("Error");
       }
     });
-  });
+  });*/
 
-  function getNames(table, column) {
-    console.log("2. inside getNames(). AJAX call starting");
-    //get names of ingredients/recipes
-    $.ajax({
-      //create an ajax request to get_names()
-      data: {
-        //data that gets sent to python
-        table: table,
-        column: column,
-      },
-      type: "POST",
-//      dataType: "json",
-      url: "/get_names",
-      success: function (result, status, xhr) {
-        console.log("3. successful ajax call");
-//        console.log("4. ajax type returned (result): ", typeof result);
-//        console.log("5. ajax type returned (result[0]): ", typeof result[0]);
-//        console.log("6. ajax result (stringify): ", JSON.stringify(result));
-        if (result) {
-          //found in database
-          console.log("7. Return result:", result); //object
-          //return JSON.parse(result);
-          return result;
-        } else {
-          console.log("Couldn't find");
-        }
-      },
-      error: function (xhr, status, error) {
-        console.log("Error??", xhr);
-      }
-    });
-  }
 
-  var names = {};
-  console.log("populating autocomplete object");
+/*  var names = {};
+  console.log("6. populating autocomplete object with returned data", mydata);
   for (var i = 0; i < 5; i++) {
     names["ape"] = null;
   }
-  console.log("8. autocomplete object: ", names);
+  console.log("7. autocomplete object: ", names);
   //http://archives.materializecss.com/0.100.2/forms.html
   $("input.autocomplete").autocomplete({
     data: names,
@@ -89,6 +93,6 @@ $(document).ready(function () {
       // Callback function when value is autcompleted.
       alert(val);
     },
-    minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
-  });
+    minLength: 1 // The minimum length of the input for the autocomplete to start. Default: 1.
+  });*/
 });
