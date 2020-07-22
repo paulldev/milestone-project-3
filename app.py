91ab8ce203c80665732c70efb05751672117efc7
Paul <paulinnorway@gmail.com>
2020-07-22 08:33:44 +0000

    Change fat column to fats

--- app.py
+++ app.py
@@ -131,7 +131,44 @@ def get_ingredient_nutrition():
 
     print(ingredient_name)
 #    sql = "SELECT  energy_amount, carbohydrate_amount, fats_amount, protein_amount, calcium_amount, zinc_amount FROM ingredient;"
-    sql = f"SELECT energy, carbohydrate, fat, protein, calcium, iron, zinc FROM ingredient WHERE name='{ingredient_name}';"
+    sql = f"SELECT energy, carbohydrate, fats, protein, calcium, iron, zinc FROM ingredient WHERE name='{ingredient_name}';"
+
+    try:
+        # Connect to the database
+        connection = pymysql.connect(host='localhost',
+                                     user=username,
+                                     password=password,
+                                     db='vmpdb')
+
+        # Run a query
+        with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+            cursor.execute(sql)
+            result = cursor.fetchall()  #returns a dictionary
+            if result:
+                print('result is:')
+                print(result)
+    finally:
+        #  Close the connection, regardless of whether or not the above was successful
+        connection.close()
+
+    return jsonify(result)
+
+
+@app.route('/save_ingredient_nutrition', methods=['POST'])
+def save_ingredient_nutrition():
+    #get data from request object
+     
+    ingredient_name = request.form['ingredient_name']
+    energy_amount = request.form['energy_amount']
+    carbohydrate_amount = request.form['carbohydrate_amount']
+    fats_amount = request.form['fats_amount']
+    protein_amount = request.form['protein_amount']
+    calcium_amount = request.form['calcium_amount']
+    iron_amount = request.form['iron_amount']
+    zinc_amount = request.form['zinc_amount']
+
+#    sql = "SELECT  energy_amount, carbohydrate_amount, fats_amount, protein_amount, calcium_amount, zinc_amount FROM ingredient;"
+    sql = f"INSERT INTO ingredient (name, energy, carbohydrate, fats, protein, calcium, iron, zinc) VALUES ('{ingredient_name}', {energy_amount}, {carbohydrate_amount}, {fats_amount}, {protein_amount}, {calcium_amount}, {iron_amount}, {zinc_amount});"
 
     try:
         # Connect to the database
