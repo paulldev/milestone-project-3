b460be9c081b7e98f858613233c1f6a92a6e5d76
Paul <paulinnorway@gmail.com>
2020-08-10 23:01:04 +0000

    test

--- app.py
+++ app.py
@@ -629,5 +629,9 @@ def get_value(table, column, name):
     return jsonify(result)
 
 
+#if __name__ == '__main__':
+#    app.run()
 if __name__ == '__main__':
-    app.run()
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
